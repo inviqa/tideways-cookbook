b82a9a031262e2b6b2de1f0458d8d27e274cb1a3
Andy Thompson <athompson@inviqa.com>
2016-01-26 12:54:11 +0000

    Update code style to satisfy foodcritic/rubocop

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['tideways']['yum_url'] = "https://s3-eu-west-1.amazonaws.com/qafoo-profiler/rpm"
+default['tideways']['yum_url'] = 'https://s3-eu-west-1.amazonaws.com/qafoo-profiler/rpm'
 default['tideways']['php_service_name'] = 'php-fpm'
 
 default['tideways']['api_key'] = nil
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
-name "tideways"
-version "0.3.0"
-maintainer "Rupert Jones"
-maintainer_email "rjones@inviqa.com"
-license "Apache 2.0"
-description "Install and configure tideways daemon and PHP extension"
-depends "yum"
+name 'tideways'
+version '0.3.0'
+maintainer 'Rupert Jones'
+maintainer_email 'rjones@inviqa.com'
+license 'Apache 2.0'
+description 'Install and configure tideways daemon and PHP extension'
+
+depends 'yum'
