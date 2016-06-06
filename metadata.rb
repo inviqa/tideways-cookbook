4eea4612ec6cceb1ba4ce0da66bb2160a9f0a25f
Felicity Ratcliffe <felicityratcliffe@Felicitys-MacBook-Pro-2.local>
2016-06-06 14:46:18 +0100

    DERBY-178 add debian family support

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name 'tideways'
-version '0.3.0'
+version '0.4.0'
 maintainer 'Rupert Jones'
 maintainer_email 'rjones@inviqa.com'
 license 'Apache 2.0'
 description 'Install and configure tideways daemon and PHP extension'
 
 depends 'yum'
+depends 'apt'
