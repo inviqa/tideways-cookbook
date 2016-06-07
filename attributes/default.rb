4ae0d774992566f58995df9a7259311ab8d3b229
Felicity Ratcliffe <felicityratcliffe@felicitys-mbp-2.home>
2016-06-07 16:49:33 +0100

    add debian family support for config file location

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,11 @@ default['tideways']['yum_url'] = 'https://s3-eu-west-1.amazonaws.com/qafoo-profi
 default['tideways']['apt_url'] = 'http://s3-eu-west-1.amazonaws.com/qafoo-profiler/packages'
 default['tideways']['gpgkey'] = 'https://s3-eu-west-1.amazonaws.com/qafoo-profiler/packages/EEB5E8F4.gpg'
 
-default['tideways']['ini_file'] = '40-tideways.ini'
+default['tideways']['ini_file'] = if platform_family?('rhel', 'fedora')
+   '40-tideways.ini'
+else
+   'tideways.ini'
+end
 
 default['tideways']['php_service_name'] = 'php-fpm'
 
